64f2282777cdb912da06a5f6d8b6f15d53f1f0d1
gurrabergh <gurrabe@hotmail.com>
2020-09-10 17:07:37 +0100

    Reuploading

---
+++ src/views/About.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="about">
+  <h1 id="mesida">mesida</h1>
+  <p>Här är <a href="https://github.com/gurrabergh/jsramverk-frontend">GitHub-repot</a> för denna sida.</p>
+  <h2 id="project-setup">Project setup</h2>
+  <pre><code>npm <span class="hljs-keyword">install</span>
+  </code></pre><h3 id="compiles-and-hot-reloads-for-development">Compiles and hot-reloads for development</h3>
+  <pre><code>npm <span class="hljs-keyword">run</span><span class="bash"> serve</span>
+  </code></pre><h3 id="compiles-and-minifies-for-production">Compiles and minifies for production</h3>
+  <pre><code>npm <span class="hljs-keyword">run</span><span class="bash"> build</span>
+  </code></pre><h3 id="lints-and-fixes-files">Lints and fixes files</h3>
+  <pre><code>npm <span class="hljs-keyword">run</span><span class="bash"> lint</span>
+  </code></pre><h3 id="customize-configuration">Customize configuration</h3>
+  <p>See <a href="https://cli.vuejs.org/config/">Configuration Reference</a>.</p>
+  </div>
+</template>
---
+++ src/views/Home.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <p>Hej! Jag heter Gustav Berg och är 23 år gammal. Född och uppvuxen i Umeå där jag fortfarande bor kvar. Jag tog studenten från Teknikprogrammet 2015 och har sedan dess jobbat på ett Ica Kvantum. Har även testat att läsa några månader på Teknisk Datavetenskap (Civilingenjör) men det kändes inte helt rätt.</p>
+<p>På min fritid så spenderar jag mycket tid på att spela ishockey då jag är målvakt i Division 2. Annars så gillar jag att styrketräna, spela datorspel, käka hamburgare eller bara att ta det lugnt i soffan framför Netflix.</p>
+<p>Jag har alltid gillat problemlösning och jag läste ett par kurser programmering på gymnasiet. Av en slump så hittade jag hemsidan till BTH's utbildning "Webbprogrammering" och efter lite efterforskningar så verkade det vara en bra utbildning. Nu ser jag fram emot att lära mig massor under de kommande två åren.</p>
+  </div>
+</template>
